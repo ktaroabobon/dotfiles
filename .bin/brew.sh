f925ce3e124787a78a8482a349e2907f6180c70e
ktaroabobon <ktaroabobon@gmail.com>
2022-09-21 19:50:47 +0900

    add: Brewfile

---
+++ .bin/brew.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+echo "Start brew.sh"
+
+if [ "$(uname)" != "Darwin" ]; then
+  echo "This script is only for macOS"
+  exit 1
+fi
+
+# Permission settings
+sudo chown -R "$(whoami)":admin /usr/local/*
+sudo chown -R g+w /usr/local/*
+
+# Run .Brewfile
+brew bundle --global
